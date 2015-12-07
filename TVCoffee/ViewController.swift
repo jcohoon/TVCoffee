02b80dcde7d651d1ab1fbddfbdff353d1b416df0
JJ Cohoon <james.cohoon@teachstone.com>
2015-12-07 16:30:23 -0500

    Initial Commit

---
+++ TVCoffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVCoffee
+//
+//  Created by James Cohoon on 12/7/15.
+//  Copyright © 2015 Alliterative Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
